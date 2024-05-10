9306e5d2053d8aa32db5fa1a276c903ed58ff723
chopper <chopper69daddy@gmail.com>
2024-05-10 12:24:01 -0600

    prod rpc

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  rpcHttpProvider: 'http://nethermind.public.dappnode:8545',
+  rpcHttpProvider: 'https://eth-mainnet.g.alchemy.com/v2/DmQk3glk8ycdD_Wvtzoklb02lNCOAcSC',
   apiUrl: 'https://verify.ethereumphunks.com/api',
 };
