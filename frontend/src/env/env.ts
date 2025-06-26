0bfbee466473454acbc79a5d76ae2ddec932425a
Snepsid <101685992+Snepsid@users.noreply.github.com>
2025-06-26 16:52:11 -0400

    Update API URL to use Railway backend

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
   rpcHttpProvider: 'https://eth-mainnet.g.alchemy.com/v2/DmQk3glk8ycdD_Wvtzoklb02lNCOAcSC',
-  apiUrl: 'https://verify.ethereumphunks.com/api',
+  apiUrl: 'https://verethfier-production.up.railway.app/api',
 };
