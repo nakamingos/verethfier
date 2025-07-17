fa5eeb66aec2d10155bfb502fbd829a600e85764
Snepsid <101685992+Snepsid@users.noreply.github.com>
2025-07-16 20:14:09 -0400

    update RPC provider and API URL in environment configuration

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  rpcHttpProvider: 'https://eth-mainnet.g.alchemy.com/v2/DmQk3glk8ycdD_Wvtzoklb02lNCOAcSC',
-  apiUrl: 'https://verify.ethereumphunks.com/api',
+  rpcHttpProvider: 'https://eth-mainnet.g.alchemy.com/v2/2ISeDpPNSILkH4lgxvgItpslW5ZupcEC',
+  apiUrl: 'https://verethfier-production.up.railway.app/api',
 };
