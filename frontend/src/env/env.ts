2f1138cfb7c2adade4832e104ff79433222fb438
Snepsid <101685992+Snepsid@users.noreply.github.com>
2026-03-13 18:15:47 -0400

    update frontend api url for renamed railway backend again

--- frontend/src/env/env.ts
+++ frontend/src/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
   rpcHttpProvider: 'https://eth-mainnet.g.alchemy.com/v2/2ISeDpPNSILkH4lgxvgItpslW5ZupcEC',
-  apiUrl: 'https://verethier-be-prod.up.railway.app/api',
+  apiUrl: 'https://verethier-be-production.up.railway.app/api',
 };
